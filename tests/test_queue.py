7b4bbbb9546b6b94af55a7638b7f01c34447fd63
stv <stv@stanford.edu>
2014-12-07 01:36:51 -0800

    hack certs

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -14,16 +14,6 @@ url_re = re.compile(r'http://example.com/.*')
 class QueueTest(unittest.TestCase):
 
     def setUp(self):
-        """
-        with patch.object(XQueuePullManager, '_request') as mock:
-            mock.return_value = {}
-            self.manager = XQueuePullManager(
-                'https://example.com/xqueue',
-                settings.QUEUE_NAME,
-                auth_basic,
-                auth_xqueue,
-            )
-        """
         @responses.activate
         def run():
             responses.add(
@@ -50,24 +40,54 @@ class QueueTest(unittest.TestCase):
         pass
 
     def test_init_fail_auth_basic(self):
+        """
+        auth_basic = (settings.QUEUE_AUTH_USER, settings.QUEUE_AUTH_PASS)
+        auth_xqueue = (settings.QUEUE_USER, settings.QUEUE_PASS)
+        manager = XQueuePullManager(
+            'http://example.com',
+            settings.QUEUE_NAME,
+            auth_basic,
+            auth_xqueue,
+        )
+        """
         pass
 
     def test_init_fail_auth_xqueue(self):
         pass
 
     def test_put(self):
+        """
+        xqueue_reply = {
+            'xqueue_header': json.dumps(xqueue_header),
+            'xqueue_body': json.dumps({
+                'action': action,
+                'download_uuid': download_uuid,
+                'verify_uuid': verify_uuid,
+                'username': username,
+                'course_id': course_id,
+                'url': download_url,
+            }),
+        }
+        self.manager.put(xqueue_reply)
+        """
         pass
 
     def test_put_fail(self):
         pass
 
     def test_pop(self):
+        """
+        certdata = self.manager.pop()
+        """
         pass
 
     def test_pop_fail(self):
         pass
 
     def test_str(self):
+        string = str(self.manager)
+        string_unicode = unicode(self.manager)
+        self.assertEquals(string, string_unicode)
         pass
 
     def test_len(self):

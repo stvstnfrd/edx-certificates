94f414e28d068ed5640e1fce247ec4e68c587abb
stv <stv@stanford.edu>
2015-07-03 22:32:56 -0700

    more cleanup

--- openedx_certificates/settings.py
+++ openedx_certificates/settings.py
@@ -41,80 +41,91 @@ def _load_json(filepath, filename):
     return result
 
 
-_ROOT_PATH = os.path.abspath(__file__)
-_ROOT_PATH = os.path.dirname(_ROOT_PATH)
-_REPO_PATH = _ROOT_PATH
-_ENV_ROOT = os.path.dirname(os.path.dirname(_REPO_PATH))
-_AUTH = _load_json(_ENV_ROOT, 'auth.json')
-_ENV = _load_json(_ENV_ROOT, 'env.json')
-_default = _load_json(_REPO_PATH, 'config.json')
+_path_file = os.path.abspath(__file__)
+_path_module = os.path.dirname(_path_file)
+_path_package = os.path.dirname(_path_module)
+_path_application = os.path.dirname(_path_package)
+
+_auth = _load_json(_path_application, 'auth.json')
+_env = _load_json(_path_application, 'env.json')
+_default = _load_json(_path_module, 'config.json')
+_custom = {}
+
+
+def get(key, default=None, course_id=None):
+    # TODO: it may be more performant to merge the dicts together once
+    def _get(key, default=None):
+        for config in [_custom, _auth, _env, _default]:
+            if key in config:
+                return config[key]
+        return default
+    if course_id:
+        if course_id in _get('CERT_DATA'):
+            if key in _get('CERT_DATA')[course_id]:
+                return _get('CERT_DATA')[course_id][key]
+    return _get(key, default)
+
+# TODO: rename
+def set(key, value):
+    _custom[key] = value
+    return value
+
+
+_CERT_BUCKET = get('CERT_BUCKET')
+if _CERT_BUCKET:
+    if not get('CERT_VERIFY_URL'):
+        set(
+            'CERT_VERIFY_URL',
+            "http://{}.s3.amazonaws.com".format(_CERT_BUCKET)
+        )
+    if not get('CERT_DOWNLOAD_URL'):
+        set(
+            'CERT_DOWNLOAD_URL',
+            "https://{}.s3.amazonaws.com".format(_CERT_BUCKET)
+        )
+
+if not get('CERT_GPG_DIR'):
+    set(
+        'CERT_GPG_DIR',
+        "{0}/.gnupg".format(
+            os.environ['HOME'],
+        )
+    )
 
 # Override _CERT_PRIVATE_DIR if you have have private templates, fonts, etc.
-_CERT_PRIVATE_DIR = _REPO_PATH
 if 'CERT_PRIVATE_DIR' in os.environ:
-    _CERT_PRIVATE_DIR = path(os.environ['CERT_PRIVATE_DIR'])
-else:
-    _CERT_PRIVATE_DIR = _ENV.get('CERT_PRIVATE_DIR')
-# TODO: add to default auth.json file for backward compat
-# else: delete
-_AUTH['QUEUE_USER'] = _AUTH.get('QUEUE_USER', 'lms')
-_CERT_BUCKET = _ENV.get('CERT_BUCKET')
-_ENV['CERT_VERIFY_URL'] = _ENV.get(
-    'CERT_VERIFY_URL',
-    'http://{}.s3.amazonaws.com'.format(_CERT_BUCKET)
-)
-_ENV['CERT_DOWNLOAD_URL'] = _ENV.get(
-    'CERT_DOWNLOAD_URL',
-    'https://{}.s3.amazonaws.com'.format(_CERT_BUCKET)
-)
-_ENV['CERT_GPG_DIR'] = _ENV.get(
-    'CERT_GPG_DIR',
-    "{0}/.gnupg".format(
-        os.environ['HOME'],
+    set('CERT_PRIVATE_DIR', path(os.environ['CERT_PRIVATE_DIR']))
+
+if get('CERT_PRIVATE_DIR') and get('TEMPLATE_DATA_SUBDIR'):
+    set(
+        u'TEMPLATE_DIR',
+        os.path.join(
+            get('CERT_PRIVATE_DIR'),
+            get('TEMPLATE_DATA_SUBDIR'),
+        )
     )
-)
-_ENV[u'TEMPLATE_DIR'] = os.path.join(
-    _CERT_PRIVATE_DIR,
-    _ENV.get('TEMPLATE_DATA_SUBDIR', ''),
-)
 
-LOGGING = get_logger_config(
-    _ENV.get('LOG_DIR', _ENV_ROOT),
-    logging_env=_ENV.get('LOGGING_ENV'),
-    local_loglevel=_ENV.get('LOCAL_LOGLEVEL'),
-    debug=_ENV.get('DEBUG'),
-    dev_env=_ENV.get('LOGGING_DEV_ENV'),
-)
-
-# Specify these credentials before running the test suite
-# or ensure that your .boto file has write permission
-# to the bucket.
-# CERT_AWS_KEY = AUTH_TOKENS.get('CERT_AWS_KEY')
-# CERT_AWS_ID = AUTH_TOKENS.get('CERT_AWS_ID')
-
-pprint(_AUTH)
-pprint(_ENV)
-pprint(_default)
-
-_CERT_DATA_FILE = _ENV.get('CERT_DATA_FILE')
-_CERT_DATA_FILE = os.path.join(
-    _CERT_PRIVATE_DIR,
-    _CERT_DATA_FILE or ''
-)
-try:
-    with open(_CERT_DATA_FILE) as file_cert_data:
-        _CERT_DATA = yaml.load(file_cert_data.read().decode('utf-8'))
-except IOError:
-    _CERT_DATA = {}
-_ENV['CERT_DATA'] = _CERT_DATA
+if get('CERT_DATA_FILE'):
+    set(
+        'CERT_DATA_FILE',
+        os.path.join(
+            get('CERT_PRIVATE_DIR'),
+            get('CERT_DATA_FILE'),
+        )
+    )
+    try:
+        with open(get('CERT_DATA_FILE')) as file_cert_data:
+            _CERT_DATA = yaml.load(file_cert_data.read().decode('utf-8'))
+    except IOError:
+        _CERT_DATA = {}
+    set('CERT_DATA', _CERT_DATA)
 
 # Locale and Translations
-
 DEFAULT_TRANSLATIONS = {
     'en_US': {
         'success_text': u'has successfully completed a free online offering of',
         'grade_interstitial': u"with {grade}.",
-        'disclaimer_text': _ENV.get('CERTS_SITE_DISCLAIMER_TEXT'),
+        'disclaimer_text': get('CERTS_SITE_DISCLAIMER_TEXT'),
         'verify_text': u"Authenticity can be verified at {verify_link}",
     },
 }
@@ -123,13 +134,20 @@ DEFAULT_TRANSLATIONS = {
 # dummy key:
 # https://raw.githubusercontent.com/edx/configuration/master/playbooks/roles/certs/files/example-private-key.txt
 
+LOGGING = get_logger_config(
+    get('LOG_DIR', _path_application),
+    logging_env=get('LOGGING_ENV'),
+    local_loglevel=get('LOCAL_LOGLEVEL'),
+    debug=get('DEBUG'),
+    dev_env=get('LOGGING_DEV_ENV'),
+)
 
-def get(key, default=None):
-    # TODO: it may be more performant to merge the dicts together once
-    for config in [_AUTH, _ENV, _default]:
-        if key in config:
-            return config[key]
-    return default
 
 # TODO: add all `CERT_*` envvars to dict
+pprint(_auth)
+pprint(_env)
+pprint(_default)
+pprint(_custom)
 print(get('DEBUG'))
+print(get('VERSION'))
+print(get('VERSION', course_id='Medicine/Sci-Write/Fall2014'))
